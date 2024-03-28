8e0aa5b92d45287d13aa84776c8d159ee5ee1de0
Ashtosh3009 <86482178+Ashtosh3009@users.noreply.github.com>
2024-03-29 02:35:12 +0530

    Create Exercise3.java

---
+++ Day-1/Calculating String Length/Exercise3.java
@@ -0,0 +1,8 @@
+public class Exercise {
+    public static int stringLength(String str){
+        // Use the length method to get the length of str
+        // your code here
+        int l = str.length();
+        return l;
+    }
+}
